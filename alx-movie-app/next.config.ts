a40d94434f6831523ce60043c0ef3fcf5adbe1cb
Talaku Getahun <getahuntalaku@gmail.com>
2025-02-09 22:12:21 +0300

    Set up next-pwa for PWA features

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -6,3 +6,15 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+const withPWA = require('@ducanh2912/next-pwa').default;
+
+module.exports = withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public', // Destination directory for the PWA files
+    register: true, // Register the PWA service worker
+    skipWaiting: true, // Skip waiting for service worker to activate
+    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  },
+});
